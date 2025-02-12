4262a4488ab8b9aea659e3650b3b2527f504cab8
Darshan R Varu <darshanvaru2003@gmail1.com>
2025-02-12 15:45:36 +0530

    Edited Paging Logic

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -14,19 +14,27 @@ class _SignInState extends State<SignIn> {
   final lastNameController = TextEditingController();
   final dateController = TextEditingController();
   final ageController = TextEditingController();
+  final addressController = TextEditingController();
+  final emailController = TextEditingController();
+  final contactController = TextEditingController();
+  final cityController = TextEditingController();
+  final countryController = TextEditingController();
+  final stateController = TextEditingController();
+  final pinCodeController = TextEditingController();
 
   // Page Controllers
   final PageController signInController = PageController();
-  final PageController signInSubController = PageController();
+  final PageController signInSub1Controller = PageController();
+  final PageController signInSub2Controller = PageController();
+  final PageController signInSub3Controller = PageController();
 
   String selectedGender = '';
-  bool isLastPage = false;
-  bool isLastSubPage = false;
+  int isLastPage = 0, isLastSub1Page = 0, isLastSub2Page = 0, isLastSub3Page = 0;
 
   @override
   void dispose() {
     signInController.dispose();
-    signInSubController.dispose();
+    signInSub1Controller.dispose();
     super.dispose();
   }
 
@@ -38,7 +46,7 @@ class _SignInState extends State<SignIn> {
         physics: const NeverScrollableScrollPhysics(),
         onPageChanged: (index) {
           setState(() {
-            isLastPage = (index == 1); // Checking if last main page
+            isLastPage = index; // Checking if last main page
           });
         },
         children: [
@@ -47,6 +55,12 @@ class _SignInState extends State<SignIn> {
           Padding(
             padding: const EdgeInsets.only(top: 40.0),
             child: _basicInfo(),
+          ),
+
+          // Contact Info Page
+          Padding(
+            padding: const EdgeInsets.only(top: 40.0),
+            child: _contactDetails(),
           ),
 
           // Success Page
@@ -97,11 +111,11 @@ class _SignInState extends State<SignIn> {
             child: Padding(
               padding: const EdgeInsets.only(top: 30.0),
               child: PageView(
-                controller: signInSubController,
+                controller: signInSub1Controller,
                 physics: const NeverScrollableScrollPhysics(),
                 onPageChanged: (index) {
                   setState(() {
-                    isLastSubPage = (index == 1); // Check last sub-page
+                    isLastSub1Page = index; // Check last sub-page
                   });
                 },
                 children: [
@@ -112,11 +126,329 @@ class _SignInState extends State<SignIn> {
                       padding: const EdgeInsets.symmetric(horizontal: 20),
                       child: Column(
                         children: [
-                          _customTextField(firstNameController, 'First Name'),
+                          const SizedBox(height: 8),
+                          //first Name
+                          TextField(
+                            controller: firstNameController,
+                            decoration: InputDecoration(
+                              labelText: 'First Name',
+                              border: const OutlineInputBorder(),
+                            ),
+                          ),
                           const SizedBox(height: 15),
-                          _customTextField(middleNameController, 'Middle Name'),
+
+                          //Middle Name
+                          TextField(
+                            controller: middleNameController,
+                            decoration: InputDecoration(
+                              labelText: 'Middle Name',
+                              border: const OutlineInputBorder(),
+                            ),
+                          ),
                           const SizedBox(height: 15),
-                          _customTextField(lastNameController, 'Last Name'),
+
+                          //Last Name
+                          TextField(
+                            controller: lastNameController,
+                            decoration: InputDecoration(
+                              labelText: 'Last Name',
+                              border: const OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  // First Sub Page - DOB, Gender
+                  SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 8),
+                          // Date of Birth Field
+                          TextField(
+                            controller: dateController,
+                            readOnly: true,
+                            decoration: InputDecoration(
+                              labelText: 'Date of Birth',
+                              border: const OutlineInputBorder(),
+                              suffixIcon: IconButton(
+                                icon: const Icon(Icons.calendar_today),
+                                onPressed: () async {
+                                  DateTime? pickedDate = await showDatePicker(
+                                    context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime(1900),
+                                    lastDate: DateTime.now(),
+                                  );
+
+                                  if (pickedDate != null) {
+                                    setState(() {
+                                      dateController.text = "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+
+                                      // Calculate Age
+                                      DateTime today = DateTime.now();
+                                      int age = today.year - pickedDate.year;
+                                      if (today.month < pickedDate.month ||
+                                          (today.month == pickedDate.month && today.day < pickedDate.day)) {
+                                        age--;
+                                      }
+                                      ageController.text = age.toString();
+                                    });
+                                  }
+                                },
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+
+                          // Age Field
+                          TextField(
+                            controller: ageController,
+                            readOnly: true,
+                            decoration: const InputDecoration(
+                              labelText: 'Age',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+
+                          // Gender Selection
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Text('Gender:', style: TextStyle(fontSize: 16)),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Radio<String>(
+                                    value: "Male",
+                                    groupValue: selectedGender,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        selectedGender = value!;
+                                      });
+                                    },
+                                  ),
+                                  const Text('Male'),
+                                  const SizedBox(width: 20),
+
+                                  Radio<String>(
+                                    value: "Female",
+                                    groupValue: selectedGender,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        selectedGender = value!;
+                                      });
+                                    },
+                                  ),
+                                  const Text('Female'),
+                                  const SizedBox(width: 20),
+
+                                  Radio<String>(
+                                    value: "Other",
+                                    groupValue: selectedGender,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        selectedGender = value!;
+                                      });
+                                    },
+                                  ),
+                                  const Text('Other'),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+
+        //Previous Button
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Align(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Align(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      signInSub1Controller.previousPage(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeIn,
+                      );
+                    },
+                    child: const Text('Previous'),
+                  ),
+                ),
+              ),
+            ),
+
+            // Next Button
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Align(
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (isLastSub1Page == 1) {
+                      signInController.nextPage(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeIn,
+                      );
+                    } else {
+                      signInSub1Controller.nextPage(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeIn,
+                      );
+                    }
+                  },
+                  child: const Text('Next'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _contactDetails() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+
+        // Main Heading (Left aligned)
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+          child: Text(
+            'Enter Your Contact Details...',
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+        ),
+
+        // Nested PageView inside Center
+        Expanded(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: PageView(
+                controller: signInSub2Controller,
+                physics: const NeverScrollableScrollPhysics(),
+                onPageChanged: (index) {
+                  setState(() {
+                    isLastSub2Page = index; // Check last sub-page
+                  });
+                },
+                children: [
+
+                  //Name Inputs
+                  SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 8),
+                          //Email
+                          TextField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              border: const OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+
+                          //Contact details
+                          TextField(
+                            controller: contactController,
+                            decoration: InputDecoration(
+                              labelText: 'Contact',
+                              border: const OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+
+                          //Address hading
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text('Address', ),
+                            ],
+                          ),
+                          Divider(),
+                          //Address
+                          // Address Field
+                          TextField(
+                            controller: addressController,
+                            minLines: 3,
+                            maxLines: null,
+                            decoration: const InputDecoration(
+                              labelText: 'Address',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+
+                          // Country and PinCode Fields
+                          Row(
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  controller: countryController,
+                                  decoration: const InputDecoration(
+                                    labelText: 'Country',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: TextField(
+                                  controller: pinCodeController,
+                                  decoration: const InputDecoration(
+                                    labelText: 'Pincode',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 15),
+
+                          // City and State Fields
+                          Row(
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  controller: cityController,
+                                  decoration: const InputDecoration(
+                                    labelText: 'City',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: TextField(
+                                  controller: stateController,
+                                  decoration: const InputDecoration(
+                                    labelText: 'State',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+
                         ],
                       ),
                     ),
@@ -225,7 +557,7 @@ class _SignInState extends State<SignIn> {
                         ],
                       ),
                     ),
-                  ), // Second Sub Page
+                  ),
                 ],
               ),
             ),
@@ -242,10 +574,21 @@ class _SignInState extends State<SignIn> {
                 child: Align(
                   child: ElevatedButton(
                     onPressed: () {
-                      signInSubController.previousPage(
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeIn,
-                      );
+                      if(isLastSub2Page == 0){
+                        signInController.previousPage(
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeIn,
+                        );
+                        signInSub1Controller.previousPage(
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeIn,
+                        );
+                      }else {
+                        signInSub2Controller.previousPage(
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeIn,
+                        );
+                      }
                     },
                     child: const Text('Previous'),
                   ),
@@ -259,17 +602,10 @@ class _SignInState extends State<SignIn> {
               child: Align(
                 child: ElevatedButton(
                   onPressed: () {
-                    if (isLastSubPage) {
-                      signInController.nextPage(
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeIn,
-                      );
-                    } else {
-                      signInSubController.nextPage(
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeIn,
-                      );
-                    }
+                    signInController.nextPage(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeIn,
+                    );
                   },
                   child: const Text('Next'),
                 ),
@@ -278,17 +614,6 @@ class _SignInState extends State<SignIn> {
           ],
         ),
       ],
-    );
-  }
-
-  // Custom Input Field Widget
-  Widget _customTextField(TextEditingController controller, String label) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-        labelText: label,
-        border: const OutlineInputBorder(),
-      ),
     );
   }
 }
