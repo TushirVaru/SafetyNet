7e50676736d1eb1061dc4b618a95aeee54f8d542
Darshan R Varu <darshanvaru2003@gmail1.com>
2025-02-15 07:40:12 +0530

    working module

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:flutter_background_service/flutter_background_service.dart';
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-// import 'package:sensors_plus/sensors_plus.dart';
-// import 'dart:async';
-
-// import 'package:rxdart/rxdart.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+import 'dart:async';
+import 'package:rxdart/rxdart.dart';
 import 'package:SafetyNet/screens/home_page.dart';
 import 'package:SafetyNet/screens/login.dart';
 import 'package:SafetyNet/screens/on_boarding.dart';
@@ -50,20 +49,17 @@ class MyApp extends StatelessWidget {
 //     android: androidSettings,
 //     iOS: DarwinInitializationSettings(requestAlertPermission: true, requestBadgePermission: true, requestSoundPermission: true),
 //   );
-//
 //   await notificationsPlugin.initialize(settings, onDidReceiveNotificationResponse: (response) {
 //     if (response.payload != null) {
 //       selectNotificationSubject.add(response.payload);
 //     }
 //   });
 // }
-//
 // Future<void> initializeService() async {
 //   const AndroidNotificationChannel channel = AndroidNotificationChannel(
 //     'safety_net_channel', 'Safety Net',
 //     description: 'Used for monitoring accidents', importance: Importance.high,
 //   );
-//
 //   await notificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.createNotificationChannel(channel);
 //   await backgroundService.configure(
 //     androidConfiguration: AndroidConfiguration(
@@ -73,30 +69,25 @@ class MyApp extends StatelessWidget {
 //     iosConfiguration: IosConfiguration(autoStart: true, onForeground: onStart),
 //   );
 // }
-//
 // @pragma('vm:entry-point')
 // void onStart(ServiceInstance service) async {
 //   SharedPreferences prefs = await SharedPreferences.getInstance();
 //   bool isLoggedIn = prefs.getBool("isLoggedIn") ?? false;
 //   if (!isLoggedIn) return;
-//
 //   service.on('stopService').listen((_) => service.stopSelf());
 //   Timer.periodic(const Duration(milliseconds: 500), (timer) async {
 //     if (!isLoggedIn) {
 //       timer.cancel();
 //       return;
 //     }
-//
 //     AccelerometerEvent? accelEvent = await accelerometerEvents.first;
 //     double acceleration = sqrt(accelEvent.x * accelEvent.x + accelEvent.y * accelEvent.y + accelEvent.z * accelEvent.z);
-//
 //     if (acceleration > 25.0) {
 //       await prefs.setBool("isAccidentDetected", true);
 //       _sendEmergencyAlert(service);
 //     }
 //   });
 // }
-//
 // Future<void> _sendEmergencyAlert(ServiceInstance service) async {
 //   await notificationsPlugin.show(0, "Emergency Alert Sent!", "Your emergency contacts have been notified.", const NotificationDetails(android: AndroidNotificationDetails('safety_net_channel', 'Emergency Alerts')));
 //   service.invoke('emergencyAlert', {'message': "Emergency alert sent!"});
